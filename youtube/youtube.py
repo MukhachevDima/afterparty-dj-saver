e376ba2c62359da0e63c0f16c66b4f918b27ddec
d.mukhachev <d.mukhachev@just-ai.com>
2023-03-22 02:14:47 +0300

    youtube

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -3,42 +3,45 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# Set up the credentials flow
-flow = InstalledAppFlow.from_client_secrets_file(
-    'client_secret.json', scopes=['https://www.googleapis.com/auth/youtube']
-)
-creds = Credentials.from_authorized_user_info(info={}, scopes=['https://www.googleapis.com/auth/youtube'])
 
+with open('client_api.txt', 'r') as file:
+    api_key = file.read().rstrip()
 # Set up the YouTube API client
-youtube = build('youtube', 'v3', credentials=creds)
+youtube = build('youtube', 'v3', developerKey=api_key)
 
-# Search for a video by name
-query = 'Your video name here'
-search_response = youtube.search().list(
-    q=query,
-    type='video',
-    part='id,snippet',
-    maxResults=10
-).execute()
+with open('../play.list', 'r') as file:
+    temp = file.read()
+    play_list = temp.split('\n')
 
-# Get the first video from the search results
-video_id = search_response['items'][0]['id']['videoId']
-video_title = search_response['items'][0]['snippet']['title']
+# Search for a video by nameimpr
+for song in play_list:
+    if song[0] ==  '#':
+        continue
+    search_response = youtube.search().list(
+        q=song,
+        type='video',
+        part='id,snippet',
+        maxResults=10
+    ).execute()
 
-# Add the video to a playlist
-playlist_id = 'Your playlist ID here'
-playlist_response = youtube.playlistItems().insert(
-    part='snippet',
-    body={
-        'snippet': {
-            'playlistId': playlist_id,
-            'resourceId': {
-                'kind': 'youtube#video',
-                'videoId': video_id
-            },
-            'title': video_title
+    # Get the first video from the search results
+    video_id = search_response['items'][0]['id']['videoId']
+    video_title = search_response['items'][0]['snippet']['title']
+
+    # Add the video to a playlist
+    playlist_id = 'PLJ_lAwHSNYgnJojhzq7Nx_tDapWzn9Dl9'
+    playlist_response = youtube.playlistItems().insert(
+        part='snippet',
+        body={
+            'snippet': {
+                'playlistId': playlist_id,
+                'resourceId': {
+                    'kind': 'youtube#video',
+                    'videoId': video_id
+                },
+                'title': video_title
+            }
         }
-    }
-).execute()
+    ).execute()
 
-print(f'The video "{video_title}" has been added to the playlist "{playlist_response["snippet"]["title"]}"')
+    print(f'The video "{video_title}" has been added to the playlist "{playlist_response["snippet"]["title"]}"')
