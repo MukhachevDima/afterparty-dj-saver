d5582a73b83a4f9b5e7d691243ca7ecdd81dc5b6
d.mukhachev <d.mukhachev@just-ai.com>
2023-03-22 03:03:02 +0300

    final

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,21 +1,22 @@
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 from googleapiclient.errors import HttpError
+from time import sleep
 
 
-with open('client_api.txt', 'r') as file:
-    api_key = file.read().rstrip()
+# Set up the credentials flow
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    'client_secret.json', scopes=['https://www.googleapis.com/auth/youtube']
+)
+creds = flow.run_console()
 # Set up the YouTube API client
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-with open('../play.list', 'r') as file:
-    temp = file.read()
-    play_list = temp.split('\n')
-
-# Search for a video by nameimpr
-for song in play_list:
-    if song[0] ==  '#':
+youtube = googleapiclient.discovery.build('youtube', 'v3', credentials=creds)
+# Search for a video by name
+i = 0
+for line in open('../play.list', 'r'):
+    song = line.strip()
+    if song.startswith('#') or song.strip() == '':
         continue
     search_response = youtube.search().list(
         q=song,
@@ -23,18 +24,18 @@ for song in play_list:
         part='id,snippet',
         maxResults=10
     ).execute()
-
     # Get the first video from the search results
     video_id = search_response['items'][0]['id']['videoId']
     video_title = search_response['items'][0]['snippet']['title']
-
+    print(video_title)
     # Add the video to a playlist
-    playlist_id = 'PLJ_lAwHSNYgnJojhzq7Nx_tDapWzn9Dl9'
+    playlist_id = 'PLJ_lAwHSNYgkU6jJTZzdTlC90TDyXj0-C'
     playlist_response = youtube.playlistItems().insert(
         part='snippet',
         body={
             'snippet': {
                 'playlistId': playlist_id,
+                'position': i,
                 'resourceId': {
                     'kind': 'youtube#video',
                     'videoId': video_id
@@ -43,5 +44,6 @@ for song in play_list:
             }
         }
     ).execute()
-
+    i += 1
+    sleep(1)
     print(f'The video "{video_title}" has been added to the playlist "{playlist_response["snippet"]["title"]}"')
